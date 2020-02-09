ccd87abe6aa1cf40859250a5a4c04cfe4fc3651f
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2020-02-08 19:53:28 -0600

    refactored controlelrs

--- api/routes/github.js
+++ api/routes/github.js
@@ -4,8 +4,15 @@ var router = express.Router();
 
 var rp = require('request-promise');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
+  res.status(200).send(
+    "Routes: <ul>\
+    <li>/profile  ::get my GitHub profile.</li>\
+    </ul>"
+  );
+})
+
+router.get('/profile', function(req, res, next) {
   const options = {
     url: 'https://api.github.com/users/awallace689',
     headers: {
